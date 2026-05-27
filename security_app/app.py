2c619d431cf9d0ca53e78ff929056a96df0cc385
Renz Castillo <118255527+Renz0432@users.noreply.github.com>
2026-05-27 23:12:09 +0800

    Update app.py

--- security_app/app.py
+++ security_app/app.py
@@ -111,4 +111,6 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Pull the port dynamic variable assigned by Railway, fallback to 5000 locally
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
